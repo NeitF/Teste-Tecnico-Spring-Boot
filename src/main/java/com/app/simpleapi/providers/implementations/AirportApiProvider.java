51510733d61b10dac909a633236166f50ef73191
Nathan Fonseca <nafs2002@hotmail.com>
2026-01-14 14:07:48 -0300

    feat: aplicando lógica de retry na busca de aeroporto em caso de falha da API externa

--- src/main/java/com/app/simpleapi/providers/implementations/AirportApiProvider.java
+++ src/main/java/com/app/simpleapi/providers/implementations/AirportApiProvider.java
@@ -4,14 +4,19 @@ import com.app.simpleapi.domain.Aeroporto;
 import com.app.simpleapi.dtos.AirportApiResponseDto;
 import com.app.simpleapi.exceptions.UserFriendlyException;
 import com.app.simpleapi.providers.interfaces.IAirportProvider;
+import com.app.simpleapi.utils.AirportApiConsts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestClient;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class AirportApiProvider implements IAirportProvider {
@@ -27,18 +32,8 @@ public class AirportApiProvider implements IAirportProvider {
 
     @Override
     public List<Aeroporto> getAirport(String codigoAeroporto) {
-        List<Aeroporto> aeroportos = new ArrayList<Aeroporto>();
-        Map<String, List<AirportApiResponseDto>> response = restClient
-                .get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("airports")
-                        .queryParam("apt", codigoAeroporto)
-                        .build())
-                .retrieve()
-                .onStatus(status -> status.isError(), (req, resp) -> {
-                    throw new UserFriendlyException("Houve um erro ao buscar pelo aeroporto");
-                })
-                .body(new ParameterizedTypeReference<Map<String, List<AirportApiResponseDto>>>() {});
+        var aeroportos = new ArrayList<Aeroporto>();
+        var response = CallApiGetAirport(codigoAeroporto);
 
         response.forEach((codigoIcao, values) ->{
             values.forEach(value -> {
@@ -52,6 +47,39 @@ public class AirportApiProvider implements IAirportProvider {
         });
 
         return aeroportos;
+    }
+
+    public Map<String, List<AirportApiResponseDto>> CallApiGetAirport(String codigoAeroporto) {
+        int tentativasMaximas = 3;
+        int tentativaAtual = 1;
+
+        while (true) {
+            try {
+                return restClient
+                        .get()
+                        .uri(uriBuilder -> uriBuilder
+                            .path("airports")
+                            .queryParam("apt", codigoAeroporto)
+                            .build())
+                        .retrieve()
+                        .body(new ParameterizedTypeReference<Map<String, List<AirportApiResponseDto>>>() {});
+
+            } catch (HttpClientErrorException | HttpServerErrorException e) {
+                if (tentativaAtual >= tentativasMaximas)
+                    throw new UserFriendlyException(AirportApiConsts.ErrorMessageErroBuscaAeroporto);
+
+                tentativaAtual++;
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException(ex);
+                }
+
+            } catch (Exception e) {
+                throw new UserFriendlyException(AirportApiConsts.ErrorMessageErroBuscaAeroporto);
+            }
+        }
     }
 
 }
---
+++ src/main/java/com/app/simpleapi/utils/AirportApiConsts.java
@@ -0,0 +1,5 @@
+package com.app.simpleapi.utils;
+
+public class AirportApiConsts {
+    public static final String ErrorMessageErroBuscaAeroporto = "Houve um erro ao buscar pelo aeroporto";
+}
